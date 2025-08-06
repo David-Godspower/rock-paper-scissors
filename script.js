614ea9321799511ca19ec54702db010bf2c0a516
Ajala David Godspower <ajaladavid11@gmail.com>
2025-08-07 00:21:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Welcome to this game of Rock Paper Scissors");
+
+// Declaring scores for both human and computer
+let humanScore = 0;
+let computerScore = 0;
+
+let counter = 0;
+
+// Function for the computer choice
+function getComputerChoice(){
+   return  Math.floor(Math.random()*3);
+}
+let computerChoice = getComputerChoice();
+if (computerChoice == 0){
+    console.log("Rock");
+}else if(computerChoice == 1){
+    console.log("Paper");
+}else{
+    console.log("Scissors");
+}
+// Function for the human choice
+function getHumanChoice(){
+    let humanChoice = prompt("Enter your choice(Rock, Paper or Scissors): ").toLowerCase();
+    return humanChoice;
+
+}
+console.log(getHumanChoice())
