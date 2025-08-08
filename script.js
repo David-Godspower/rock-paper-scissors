0f20e49872f0577d7ee529cd1feb83b243ce9c0c
Ajala David Godspower <ajaladavid11@gmail.com>
2025-08-08 04:25:32 +0100

    Update script.js to the latest update

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ console.log("Welcome to this game of Rock Paper Scissors");
 let humanScore = 0;
 let computerScore = 0;
 
+// Prompt to get the user's name
 
-alert('Rock wins over scissors\n Paper wins over Rock\n Scissors wins Paper');
+let name = prompt("Enter your name: ").toUpperCase().trim();
+while (name == null || name.length < 3 || name.length > 20){
+    alert("Please enter a valid name with at least 3 characters and must not be greater than 20 characters.");
+    name = prompt("Enter your name: ").toUpperCase().trim();
+    
+}
+alert("Hello " + name + ", let's play Rock Paper Scissors!");
+
+// Alert to explain the rules of the game
+alert('Rock wins over scissors\n Paper wins over Rock\n Scissors wins over Paper\n Each round you will get 10 points for winning and 5 points for a tie.\n You will play 5 rounds in total. Good luck!');
 // Function for the computer choice
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"];
@@ -16,15 +26,16 @@ let computerChoice = getComputerChoice();
 
 // Function for the human choice
 function getHumanChoice(){
-    let humanChoice = prompt("Enter your choice(Rock, Paper or Scissors): ").toLowerCase();
-    if((humanChoice == "rock") || (humanChoice == "paper") || (humanChoice == "scissors")){
-    return humanChoice;
-    }else{
-         alert("Please enter a valid choice: Rock Paper Scissors");
-        return getHumanChoice();
+     let humanChoice;
+     while(true){
+        humanChoice = prompt("Enter your choice(Rock, Paper or Scissors): ").toLowerCase();
+        if((humanChoice == "rock") || (humanChoice == "paper") || (humanChoice == "scissors")){
+        return humanChoice;
+        }else{
+            alert("Please enter a valid choice: Rock Paper Scissors");
     }
 }
-
+}
 
 // Play round
 function playRound(humanChoice, computerChoice){
@@ -32,6 +43,8 @@ function playRound(humanChoice, computerChoice){
         console.log("Your choice = " + humanChoice);
          console.log("Computer choice = " + computerChoice);
         console.log("It's a tie\n");
+        humanScore+=5;
+        computerScore+=5;
     }else if(humanChoice == "rock" && computerChoice == "scissors"){
         console.log("Your choice = " + humanChoice);
          console.log("Computer choice = " + computerChoice);
@@ -66,11 +79,12 @@ function playRound(humanChoice, computerChoice){
 
 }
 
-playRound(humanChoice, computerChoice);
 
+
+// Fuction to play the game for 5 rounds
 
 function playGame(){
-    for(counter = 0; counter<5; counter++){
+    for(let counter = 0; counter<5; counter++){
         let humanChoice = getHumanChoice();
         let computerChoice = getComputerChoice();
         playRound(humanChoice, computerChoice);
@@ -79,17 +93,16 @@ function playGame(){
     console.log("Your Score: "+ humanScore);
     console.log("Computer Score: "+ computerScore);
     if(humanScore > computerScore){
-        console.log("You are the Overall Winner of this game!🥇🥇🥇");
+        alert("Congratulations " + name + ". You are the Overall Winner of this game!🥇🥇🥇");
         
     }else if(computerScore > humanScore){
-        console.log("Computer is the Overall Winner 🥇🥇🥇! You can do better next time");
+        alert("Computer is the Overall Winner 🥇🥇🥇! You can do better next time");
     }else{
-        console.log("It's a Tie Overall! You tried!");
+        alert("It's a Tie Overall! You tried!");
     }
 
 }
 playGame();
-
 
 
 
